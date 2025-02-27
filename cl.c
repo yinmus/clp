914acd5e507a511b83fab8de0f9e9fbcdd761a27
kliscle <lachin.akhmetov@gmail.com>
2025-02-27 22:47:26 +0500

    main

--- cl.c
+++ cl.c
@@ -46,24 +46,36 @@ int get_key(int timeout) {
 
 int main(int argc, char *argv[]) {
     if (argc != 3 || strcmp(argv[1], "-c") != 0) {
+        fprintf(stderr, "Usage: %s -c <filename>\n", argv[0]);
         return 1;
     }
 
     char *filename = argv[2];
 
     if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+        fprintf(stderr, "SDL initialization failed: %s\n", SDL_GetError());
         return 1;
     }
+
     if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+        fprintf(stderr, "Mixer initialization failed: %s\n", Mix_GetError());
+        SDL_Quit();
         return 1;
     }
 
     Mix_Music *music = Mix_LoadMUS(filename);
     if (!music) {
+        fprintf(stderr, "Failed to load music: %s\n", Mix_GetError());
+        Mix_CloseAudio();
+        SDL_Quit();
         return 1;
     }
 
     if (Mix_PlayMusic(music, 1) == -1) {
+        fprintf(stderr, "Failed to play music: %s\n", Mix_GetError());
+        Mix_FreeMusic(music);
+        Mix_CloseAudio();
+        SDL_Quit();
         return 1;
     }
 
@@ -71,15 +83,15 @@ int main(int argc, char *argv[]) {
     Mix_VolumeMusic(volume);
 
     int running = 1, is_paused = 0;
-    int total_time = Mix_MusicDuration(music) * 1000;
+    int total_time = Mix_MusicDuration(music); // in seconds
     int key;
     int seek_step = 5;
 
     printf("\n");
 
     while (running && Mix_PlayingMusic()) {
-        int elapsed = Mix_GetMusicPosition(NULL) * 1000;
-        print_ui(elapsed / 1000, total_time / 1000, (volume * 100) / MIX_MAX_VOLUME, is_paused);
+        int elapsed = Mix_GetMusicPosition(music); // in seconds
+        print_ui(elapsed, total_time, (volume * 100) / MIX_MAX_VOLUME, is_paused);
 
         key = get_key(1);
         switch (key) {
@@ -109,16 +121,24 @@ int main(int argc, char *argv[]) {
                 running = 0;
                 break;
             case 'h':
-                Mix_SetMusicPosition(Mix_GetMusicPosition(NULL) - seek_step);
+                if (Mix_SetMusicPosition(Mix_GetMusicPosition(music) - seek_step) == -1) {
+                    fprintf(stderr, "Seek not supported for this format\n");
+                }
                 break;
             case 'l':
-                Mix_SetMusicPosition(Mix_GetMusicPosition(NULL) + seek_step);
+                if (Mix_SetMusicPosition(Mix_GetMusicPosition(music) + seek_step) == -1) {
+                    fprintf(stderr, "Seek not supported for this format\n");
+                }
                 break;
             case 'H':
-                Mix_SetMusicPosition(Mix_GetMusicPosition(NULL) - 10);
+                if (Mix_SetMusicPosition(Mix_GetMusicPosition(music) - 10) == -1) {
+                    fprintf(stderr, "Seek not supported for this format\n");
+                }
                 break;
             case 'L':
-                Mix_SetMusicPosition(Mix_GetMusicPosition(NULL) + 10);
+                if (Mix_SetMusicPosition(Mix_GetMusicPosition(music) + 10) == -1) {
+                    fprintf(stderr, "Seek not supported for this format\n");
+                }
                 break;
         }
     }
